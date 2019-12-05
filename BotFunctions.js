0090804b4319f8a9254dcd0690d0f5dd9b6f0c10
Arkadiusz Pasek <thevik444@gmail.com>
2019-12-05 17:39:02 +0100

    Implemented core functionality

---
+++ BotFunctions.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const config = require('./config.json');
+
+exports.playSound = (_channel, _channelConnection, number, fileMap) => {
+    if(_channel == undefined || _channelConnection == undefined) {
+        console.log('Cannot play anything: Undefined!');
+        return;
+    }
+    const file = fileMap.get(number);
+    if(file == undefined) {
+        console.log('Didn\'t read any file');
+        return;
+    }
+    console.log(`Playing : ${file}...`);
+    const dispatcher = _channelConnection.playFile(config.soundsLocation + file);
+    dispatcher.on('end', () => {
+        console.log('Playing finished');
+    });
+    return dispatcher;
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.findChannel = botClient => {
+    return botClient.channels.find(channel => {
+        if(!(channel instanceof Discord.VoiceChannel) || channel.members == undefined) return false;
+
+        return channel.members.get('494922928929112064') !== undefined;
+    });
+};
+
+exports.joinChannel = async channel => {
+    try{
+        const _connection = await channel.join();
+        console.log('Joined channel');
+        return _connection;
+    }
+    catch(e) {
+        console.log(e);
+    }
+};
+
+exports.loadFileList = () => {
+    const fileMap = new Map();
+    fs.readdir(config.soundsLocation, (err, files) => {
+        if(err) throw err;
+
+        files.forEach(file => {
+            fileMap.set(fileMap.size, file);
+        });
+    });
+    console.log('Files loaded successfully');
+    return fileMap;
+};
--- index.js
+++ index.js
@@ -1,14 +1,70 @@
 const config = require('./config.json');
+const bot = require('./BotFunctions.js');
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const stdin = process.stdin;
+stdin.setEncoding('utf-8');
+
+let isReady = false;
+let channelConnection;
+let channel;
+let dispatcher;
+
+let fileMap = bot.loadFileList();
+
+console.log('Connecting...');
+
 client.once('ready', () => {
-    console.log('ready!');
+    console.log('Connected');
+    isReady = true;
 });
 
-client.on('message', message => {
-	console.log(message.content);
+// client.on('message', message => {
+// });
+
+stdin.on('data', async data => {
+    if(!isReady) return;
+
+    const re = /^bot/;
+    if(!re.test(data)) return;
+
+    data = data.replace(re, '').trim();
+    console.log(data);
+
+    switch (data) {
+        case 'leave':
+            channel.leave();
+            channel = undefined;
+            break;
+        case 'come':
+            channel = bot.findChannel(client);
+            if(channel) channelConnection = await bot.joinChannel(channel);
+            break;
+        case 'list':
+            fileMap.forEach((file, number) => console.log(`${number}-${file}`));
+            break;
+        case 'load':
+            fileMap = bot.loadFileList();
+            break;
+        case 'stop':
+        case 'end':
+            if(!dispatcher) return;
+            dispatcher.end();
+            break;
+        case 'exit':
+            process.exit();
+    }
+    if(data.startsWith('play')) {
+        if(!new RegExp('^play:.+').test(data)) {
+            console.log('Incorrect usage <play>:<number>');
+            return;
+        }
+
+        dispatcher = bot.playSound(channel, channelConnection, parseInt(data.split(':')[1]), fileMap);
+    }
+
 });
 
 client.login(config.token);
