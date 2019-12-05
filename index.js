bc70052e1b9e313eaa3c9b1d838d7701b66a2e0d
Arkadiusz Pasek <thevik444@gmail.com>
2019-12-05 12:32:06 +0100

    Initial

---
+++ index.js
@@ -0,0 +1,14 @@
+const config = require('./config.json');
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('ready!');
+});
+
+client.on('message', message => {
+	console.log(message.content);
+});
+
+client.login(config.token);
